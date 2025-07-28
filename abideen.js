501b91600f9c52e1e2d274b17a7f781f0559b678
Atijani10 <abideen.tijani10@gmail.com>
2025-07-28 06:16:40 -0500

    first commit

---
+++ abideen.js
@@ -0,0 +1,20 @@
+//var my_name = "Abideen"
+//console.log(my_name)
+
+//var myCountry = "Nigeria"
+//console.log(myCountry)
+
+//var mySchool = "unilag"
+//var level = "200 level"
+//var year = 2025
+//var output = "By "+ year + ", i'll be in " + level + " in " + mySchool
+//console.log(output)
+
+
+
+var myFrend = "Segun"
+var hisNationality = "Nigeria"
+var kid = 2
+var profession = "AgbaTester"
+const tola = "my guy's name is " + myFrend + ", he has " + kid + " kids in " + hisNationality + " his nickname is " + profession 
+console.log(tola)
