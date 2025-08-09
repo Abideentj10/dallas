71491a17c75bde031a5365dab260372f4d8ee6a7
Atijani10 <abideen.tijani10@gmail.com>
2025-08-09 16:42:05 -0500

    myjswork

--- abideen.js
+++ abideen.js
@@ -17,4 +17,90 @@ var hisNationality = "Nigeria"
 var kid = 2
 var profession = "AgbaTester"
 const tola = "my guy's name is " + myFrend + ", he has " + kid + " kids in " + hisNationality + " his nickname is " + profession 
-console.log(tola)
+//console.log(tola)
+
+
+var friends = ["segun", "lanre", "tunde", "kareem"]
+var myBestfriend = (friends[1])
+//console.log(friends)
+//console.log(myBestfriend)
+
+//friends[2] = "sam" 
+//console.log(friends)
+//friends.push("abbey")
+//console.log(friends)
+//friends.unshift("abbey")
+//console.log(friends)
+
+
+const profile = { firstName: "Abideen",
+lastName: "Tijani",
+gender: "Male",
+noOfChidren: 2,
+height: "5ft10in",
+colorOfEyes: "Brown"
+}
+//console.log(profile)
+
+
+//ASSIGNMENT
+
+//let count = 5
+//let ans = ++count
+//console.log(ans)
+
+// let count = 5
+// let result = --count
+// console.log(result)
+
+//COMPARISON
+
+// let count = 5
+// let input = "5"
+// let res = count>input
+// console.log(count==input)
+
+// console.log(count===input)
+
+// console.log(count!=input)
+
+// console.log(count!==input)
+
+// console.log(count>input)
+
+// console.log(count<input)
+
+// console.log(count>=input)
+
+// console.log(count<=input)
+
+// function multiply(x,y){
+//     console.log(x*y)
+// }
+
+// multiply(5,7)
+// multiply(8,10)
+// multiply(12,13)
+
+
+// let myNumber = 4;
+// if (myNumber < 5 || myNumber>50) {
+//     console.log("i am available")
+// }
+
+
+// let num = 9;
+// if (num >10 && num <20){
+//     console.log("we are in")
+// }
+// else{
+//     console.log("we are out")
+// }
+
+let fig = 9
+if (fig !=10 && fig >0){
+    console.log("correct")
+}
+else{
+    console.log("incorrect")
+}
